e11cd874b861b1e6046ffeb89d1bcd130b3376b7
Glaaacy <112131798+Glaaacy@users.noreply.github.com>
2022-08-26 11:19:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -48,20 +48,20 @@ bot.on('message', message => {
     }
   });
 
-
+  
   bot.on('message', message => {
-    if (message.content.startsWith("*test"))
-    if (message.member.hasPermission("BAN_MEMBERS")) {
-      message.delete()
-      if (message.members.mentions.first()) {
-          try {
-             message.channel.send("Ok.")
-          } catch {
-            message.reply("Non.");
-          }
-      } else {
-        message.reply(":x: ``Tu ne peux pas.``");
-      }
-  }
-  });
-
+  if (message.content.startsWith("*test"))
+    if (!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send("Invalid Permissions")
+    let User = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
+    if (!User) return message.channel.send("Invalid User")
+    let banReason = args.join(" ").slice(22);
+    if (!banReason) {
+    banReason = "None"
+    }
+    User.kick({reason: banReason})
+    const person = message.mentions.users.first()
+    const embed = new discord.MessageEmbed()
+        .setTitle(person.username + " got kicked by " + message.author.username)
+        .setThumbnail("https://www.progressivecombatsystems.com/wp-content/uploads/2019/03/groin-kick.jpg")
+    message.channel.send(embed)
+});
